0aa3bc343c67974511a53b5bcdfae9f1e79d759e
itsrohanhere <rohankha332@gmail.com>
2025-12-14 20:43:39 +0530

    added program to create an array, print and indert at given index

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,10 +1,38 @@
 package Array;
 
 public class Array {
-    public static class Array {
-        public static void main(String[] args) {
-            // here we do the DSA ques and topics of Array.Main.Array
+    public static void main(String[] args) {
+        // creating an array
+        int[] arr = new int[10];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+
+        // printing arrays element with a method
+        printArray(arr);
+
+        // inserting element at a mid position
+        insertAtMid(arr,3,10);
+        printArray(arr);
+
+    }
+
+    static void printArray(int[] arr) {
+        for(int i = 0 ; i < arr.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static void insertAtMid(int[] arr, int index, int value) {
+        // first we have to shift the elements one time
+        for(int i = arr.length - 2 ; i >= index ; i--){
+            arr[i+1] = arr[i];
 
         }
+        arr[index] = value;
     }
+
 }
