a49be6e42f61aea7abdf9143393335caaaff1fa8
itsrohanhere <rohankha332@gmail.com>
2025-12-14 20:33:59 +0530

    created an package array and added main class as Array

--- src/Array.java
+++
@@ -1,6 +0,0 @@
-public class Array {
-    public static void main(String[] args) {
-        // here we do the DSA ques and topics of Array
-
-    }
-}
---
+++ src/Array/Array.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Array {
+    public static class Array {
+        public static void main(String[] args) {
+            // here we do the DSA ques and topics of Array.Main.Array
+
+        }
+    }
+}
