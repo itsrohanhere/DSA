b1c2cbe7b5d52fe34a477efe3e3d5ae02d54bb43
itsrohanhere <rohankha332@gmail.com>
2025-12-15 13:48:23 +0530

    added binary search for decreasing order

--- src/Array/BinarySearch.java
+++ src/Array/BinarySearch.java
@@ -2,8 +2,10 @@ package Array;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {22,23,25,34,56,65,87,88,89,95};
-        binarySearch(arr,25);
+//        int[] arr = {22,23,25,34,56,65,87,88,89,95};
+        int[] arr2 = {99,85,74,65,42,32,20,1};
+//        binarySearch(arr,25);
+        binarySearchDesc(arr2,20);
     }
     static void binarySearch(int[] arr, int target){
         int result = -1;
@@ -15,6 +17,28 @@ public class BinarySearch {
                 System.out.println("element found at index: " + mid);
                 return;
             }else if(arr[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        if(result == -1){
+            System.out.println("element not found");
+        }else{
+            System.out.println("element found at index: " + result);
+        }
+    }
+
+    static void binarySearchDesc(int[] arr, int target){
+        int result = -1;
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] == target){
+                System.out.println("element found at index: " + mid);
+                return;
+            }else if(arr[mid] > target){
                 start = mid + 1;
             }else{
                 end = mid - 1;
